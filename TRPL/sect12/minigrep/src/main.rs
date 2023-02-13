8594dc091ae99e159e09818c5f36bac93399e959
toreis-up <toreis@reisan.dev>
2023-02-14 06:00:09 +0900

    feat: sect12

--- TRPL/sect12/minigrep/src/main.rs
+++ TRPL/sect12/minigrep/src/main.rs
@@ -1,38 +1,24 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+extern crate minigrep;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("App Error: {}", e);
+        process::exit(-1);
+    };
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config { query, filename })
-    }
-}
