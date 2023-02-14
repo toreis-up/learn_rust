a54417c9505aa8f40c2c3b2bfea5e655a1a36491
toreis-up <toreis@reisan.dev>
2023-02-14 11:16:06 +0900

    feat: sect13 -(impl)> sect12

--- TRPL/sect12/minigrep/src/main.rs
+++ TRPL/sect12/minigrep/src/main.rs
@@ -5,9 +5,7 @@ extern crate minigrep;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         // 引数解析時に問題
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
